475832fbb7653dd4d84355990705dd577db3d546
Antonin <antonin.info@protonmail.com>
2021-03-01 17:12:36 +0100

    Ajout script python génération de fichier changement de variables

---
+++ generate_vagrantfile.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+# Script lancé par Jenkins
+
+# python generate_vagrantfile.py vm_name vm_type ip_address memory cpu shared_folder
+# python generate_vagrantfile.py my_vm debian 192.168.10.10 4000 4 true
+
+import sys
+import shutil
+
+def main():
+    
+    if len(sys.argv) != 7:
+        print("Le nombre d'arguments est inexacte.")
+        sys.exit(1)
+
+    # Vagrantfile
+    filename = "Vagrantfile"
+    new_filename = "Vagrantfile_to_use"
+    shutil.copy(filename, new_filename)
+    
+    vm_name = sys.argv[1]
+    vm_type = sys.argv[2]
+    ip_address = sys.argv[3]
+    memory = sys.argv[4]
+    cpu = sys.argv[5]
+    shared_folder = sys.argv[6]
+
+    with open(new_filename, "r") as file :
+        filedata = file.read()
+
+    filedata = filedata.replace("vm_name", vm_name) \
+        .replace("vm_type", vm_type) \
+        .replace("vm_ip_address", ip_address) \
+        .replace("vm_memory", memory) \
+        .replace("vm_cpu", cpu) \
+        .replace("vm_shared_folder", shared_folder)
+
+    with open(new_filename, "w") as file:
+        file.write(filedata)
+
+    # Ansible hosts file
+    filename = "ansible/hosts"
+    new_filename = "ansible/hosts_to_use"
+    shutil.copy(filename, new_filename)
+
+    with open(new_filename, "r") as file :
+        filedata = file.read()
+
+    filedata = filedata.replace("vm_ip_address", ip_address)
+
+    with open(new_filename, "w") as file:
+        file.write(filedata)
+
+if __name__ == "__main__":
+    main()
