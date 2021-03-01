3d76a802cd316dd8ec17780ffdcec9f1a7b4c583
Antonin <antonin.info@protonmail.com>
2021-03-01 21:17:36 +0100

    Gestion des playbook simples dans installation au préalable des VM

--- generate_vagrantfile.py
+++ generate_vagrantfile.py
@@ -9,48 +9,63 @@ import sys
 import shutil
 
 def main():
-    
-    if len(sys.argv) != 7:
-        print("Le nombre d'arguments est inexacte.")
-        sys.exit(1)
 
-    # Vagrantfile
-    filename = "Vagrantfile"
-    new_filename = "Vagrantfile_to_use"
-    shutil.copy(filename, new_filename)
-    
-    vm_name = sys.argv[1]
-    vm_type = sys.argv[2]
-    ip_address = sys.argv[3]
-    memory = sys.argv[4]
-    cpu = sys.argv[5]
-    shared_folder = sys.argv[6]
+    if len(sys.argv) == 2:
 
-    with open(new_filename, "r") as file :
-        filedata = file.read()
+        ip_address = sys.argv[1]
 
-    filedata = filedata.replace("vm_name", vm_name) \
-        .replace("vm_type", vm_type) \
-        .replace("vm_ip_address", ip_address) \
-        .replace("vm_memory", memory) \
-        .replace("vm_cpu", cpu) \
-        .replace("vm_shared_folder", shared_folder)
+        # Ansible hosts file
+        filename = "ansible/hosts"
+        new_filename = "ansible/hosts_to_use"
+        shutil.copy(filename, new_filename)
 
-    with open(new_filename, "w") as file:
-        file.write(filedata)
+        with open(new_filename, "r") as file :
+            filedata = file.read()
 
-    # Ansible hosts file
-    filename = "ansible/hosts"
-    new_filename = "ansible/hosts_to_use"
-    shutil.copy(filename, new_filename)
+        filedata = filedata.replace("vm_ip_address", ip_address)
 
-    with open(new_filename, "r") as file :
-        filedata = file.read()
+        with open(new_filename, "w") as file:
+            file.write(filedata)
 
-    filedata = filedata.replace("vm_ip_address", ip_address)
+    elif len(sys.argv) == 7:
 
-    with open(new_filename, "w") as file:
-        file.write(filedata)
+        # Vagrantfile
+        filename = "Vagrantfile"
+        new_filename = "Vagrantfile_to_use"
+        shutil.copy(filename, new_filename)
+        
+        vm_name = sys.argv[1]
+        vm_type = sys.argv[2]
+        ip_address = sys.argv[3]
+        memory = sys.argv[4]
+        cpu = sys.argv[5]
+        shared_folder = sys.argv[6]
+
+        with open(new_filename, "r") as file :
+            filedata = file.read()
+
+        filedata = filedata.replace("vm_name", vm_name) \
+            .replace("vm_type", vm_type) \
+            .replace("vm_ip_address", ip_address) \
+            .replace("vm_memory", memory) \
+            .replace("vm_cpu", cpu) \
+            .replace("vm_shared_folder", shared_folder)
+
+        with open(new_filename, "w") as file:
+            file.write(filedata)
+
+        # Ansible hosts file
+        filename = "ansible/hosts"
+        new_filename = "ansible/hosts_to_use"
+        shutil.copy(filename, new_filename)
+
+        with open(new_filename, "r") as file :
+            filedata = file.read()
+
+        filedata = filedata.replace("vm_ip_address", ip_address)
+
+        with open(new_filename, "w") as file:
+            file.write(filedata)
 
 if __name__ == "__main__":
     main()
